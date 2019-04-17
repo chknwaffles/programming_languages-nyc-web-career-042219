2380b97d41d49a5128fa5f3bb50ecf3e08cca7f4
Won Kim <wonsae.kim@gmail.com>
2019-04-17 04:26:08 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,7 +9,7 @@ def reformat_languages(languages)
       type.each do |k, v|
         new_hash[lang] = {k => v}
         if new_hash[lang][:style] == nil
-          new_hash[lang][:style] == [languages]
+          new_hash[lang][:style] = [languages]
           binding.pry
         else
           binding.pry
