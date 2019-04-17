4050369145e6b0fd136215533b9c1f696e4a103a
Won Kim <wonsae.kim@gmail.com>
2019-04-17 03:49:59 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,6 +8,9 @@ def reformat_languages(languages)
     #style is the hash
     style.each do |lang, type|
       #type is value not another hash, make another hash to add value
+      type.each do |k, v|
+        new_hash[lang][k] = v
+      end
       new_hash[lang]
       binding.pry
       
