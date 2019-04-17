8d31e4639baab13127a188f425d9122b525ee3c5
Won Kim <wonsae.kim@gmail.com>
2019-04-17 04:19:51 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,13 +3,13 @@ require 'pry'
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  #style_arr = []
+  style_arr = []
   languages.each do |languages, style|
     style.each do |lang, type|
       type.each do |k, v|
         new_hash[lang] = {k => v}
         
-        if style_arr.includes()
+        if style_arr.include?()
       end
       new_hash[lang][:style] = [languages]
     end
